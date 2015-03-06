678d921e1ec0367efd271b5b2072b965c946e1e7
root <root@tester.jesin.in>
2015-03-06 19:35:29 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+JavaScript Spam Blocker
+=======================
+
+[Download](https://github.com/jesinwp/wp-blockspam-js/archive/stable.zip) the plugin, upload and activate it. Watch spam numbers coming down!
+
+<http://jesin.tk/prevent-wordpress-comment-spam-using-javascript/>
---
+++ plugin.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: JavaScript Spam Blocker
+Plugin URI: http://jesin.tk/prevent-wordpress-comment-spam-using-javascript/
+Description: Block comment spam bots with a single line of JavaScript
+Author: Jesin
+Author URI: http://jesin.tk
+*/
+
+add_action( 'preprocess_comment' , 'preprocess_comment_handler' );
+
+function preprocess_comment_handler( $commentdata ) {
+	if ( ! is_admin() && ( ! isset( $_POST['js_field'] ) || $commentdata['comment_post_ID'] != $_POST['js_field'] ) ) {
+		wp_die( 'Please enable JavaScript to comment on this blog' );
+	}
+	return $commentdata;
+}
+
+add_action( 'wp_footer', 'js_commentform_hidden_field' );
+
+function js_commentform_hidden_field() {
+?>
+<script type="text/javascript">
+var commentform = document.getElementById('commentform');
+if ( commentform ) {
+	commentform.innerHTML += '<input type="hidden" name="js_field" value="' + document.getElementById('comment_post_ID').value + '" />';
+}
+</script>
+<?php
+}
